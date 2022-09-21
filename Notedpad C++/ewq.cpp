81afcff8bfb884a2bde23360d3b7c283115f9348
Daniel De Torres <88454993+dangrishh@users.noreply.github.com>
2022-09-21 12:58:37 +0800

    Add files via upload

---
+++ Notedpad C++/ewq.cpp
@@ -0,0 +1,29 @@
+/*#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 10;
+	int y = 5;
+	bool z = x < y;
+	
+	cout << " Value of Z is: "<< x;
+	
+	
+} */
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+  int a = 10;
+  int b = 5;
+  bool z = a + b;
+  
+  cout << z;
+   
+  return 0;
+}
